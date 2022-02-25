7414d1f60599b1bcf7f0cf14b72c24627c84013e
BebeDlaStreat <bebe.vandroux@gmail>
2022-02-25 21:40:03 +0100

    try other method

--- src/main/java/fr/endoskull/api/Main.java
+++ src/main/java/fr/endoskull/api/Main.java
@@ -1,7 +1,11 @@
 package fr.endoskull.api;
 
+import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
 import fr.endoskull.api.commons.server.ServerState;
 import fr.endoskull.api.commons.server.ServerType;
 import fr.endoskull.api.data.redis.JedisAccess;
@@ -29,6 +33,7 @@ import redis.clients.jedis.JedisPubSub;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 public class Main extends JavaPlugin {
@@ -137,7 +142,42 @@ public class Main extends JavaPlugin {
 
         if (Bukkit.getPluginManager().getPlugin("ProtocolLib") != null) {
             ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-            manager.addPacketListener(new TabListener(manager));
+            manager.addPacketListener(new PacketAdapter(this, new PacketType[] { PacketType.Play.Client.TAB_COMPLETE })
+            {
+                @SuppressWarnings("rawtypes")
+                public void onPacketReceiving(PacketEvent event) {
+                    if ((event.getPacketType() == PacketType.Play.Client.TAB_COMPLETE)
+                            && (!event.getPlayer().hasPermission("hideandcustomplugins.bypass"))
+                            && (((String)event.getPacket().getStrings().read(0)).startsWith("/"))
+                            && (((String)event.getPacket().getStrings().read(0)).split(" ").length == 1)) {
+
+                        event.setCancelled(true);
+
+                        List<?> list = new ArrayList();
+                        List<?> extra = new ArrayList();
+
+                        String[] tabList = new String[list.size() + extra.size()];
+
+                        for (int index = 0; index < list.size(); index++) {
+                            tabList[index] = ((String)list.get(index));
+                        }
+
+                        for (int index = 0; index < extra.size(); index++) {
+                            tabList[(index + list.size())] = ('/' + (String)extra.get(index));
+                        }
+                        PacketContainer tabComplete = manager.createPacket(PacketType.Play.Server.TAB_COMPLETE);
+                        tabComplete.getStringArrays().write(0, tabList);
+
+                        try {
+                            manager.sendServerPacket(event.getPlayer(), tabComplete);
+                        } catch (InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+
+            });
         }
 
         super.onEnable();
