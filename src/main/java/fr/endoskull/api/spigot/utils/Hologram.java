47ce1ac604b15793a5a78e9819a95e5b74657113
BebeDlaStreat <bebe.vandroux@gmail>
2022-02-27 10:14:26 +0100

    try hologram set marker 2

--- src/main/java/fr/endoskull/api/spigot/utils/Hologram.java
+++ src/main/java/fr/endoskull/api/spigot/utils/Hologram.java
@@ -1,8 +1,10 @@
 package fr.endoskull.api.spigot.utils;
 
+import net.minecraft.server.v1_8_R3.EntityArmorStand;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
@@ -307,7 +309,6 @@ public class Hologram {
         Method setNoGravity = entityObject.getClass().getMethod("setGravity", boolean.class); // Previously setGravity(boolean) prior to 1.10
         Method setLocation = entityObject.getClass().getMethod("setLocation", double.class, double.class, double.class, float.class, float.class);
         Method setInvisible = entityObject.getClass().getMethod("setInvisible", boolean.class);
-        Method setMarker = entityObject.getClass().getMethod("setMarker", boolean.class);
 
         // Setting the properties
         setCustomName.invoke(entityObject, text);
@@ -315,7 +316,9 @@ public class Hologram {
         setNoGravity.invoke(entityObject, true);
         setLocation.invoke(entityObject, location.getX(), location.getY(), location.getZ(), 0.0F, 0.0F);
         setInvisible.invoke(entityObject, true);
-        setMarker.invoke(entityObject, true);
+        EntityArmorStand entityAs = (EntityArmorStand) entityObject;
+        ArmorStand as = (ArmorStand) entityAs;
+        as.setMarker(true);
     }
 
     /**
