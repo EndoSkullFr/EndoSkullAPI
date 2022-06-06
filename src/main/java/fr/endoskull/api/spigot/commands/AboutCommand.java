1a59d35f6b23ebccb743b19727339f2f2c12c3e8
BebeDlaStreat <bebe.vandroux@gmail>
2022-06-06 15:54:13 +0200

    rechange 2

--- src/main/java/fr/endoskull/api/spigot/commands/AboutCommand.java
+++ src/main/java/fr/endoskull/api/spigot/commands/AboutCommand.java
@@ -2,6 +2,7 @@ package fr.endoskull.api.spigot.commands;
 
 import fr.endoskull.api.Main;
 import fr.endoskull.api.commons.EndoSkullAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,7 +10,7 @@ import org.bukkit.command.CommandSender;
 public class AboutCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        sender.sendMessage(EndoSkullAPI.LINE + "\n§bEndoSkull §7est développé et géré par §b§o@BebeDlaStreat\n§7Serveur: §e" + Main.getInstance().getServer().getVersion() + "\n§7Proxy: §eWaterfall 1.18\n§7Nous n'avons pas que des plugins fait par nous mais nous essayons de faire nous même les features les plus importantes du serveur\n" + EndoSkullAPI.LINE);
+        sender.sendMessage(EndoSkullAPI.LINE + "\n§bEndoSkull §7est développé et géré par §b§o@BebeDlaStreat\n§7Serveur: §e" + Main.getInstance().getServer().getName() + "\n§7Proxy: §eWaterfall 1.18\n§7Nous n'avons pas que des plugins fait par nous mais nous essayons de faire nous même les features les plus importantes du serveur\n" + EndoSkullAPI.LINE);
         return false;
     }
 }
