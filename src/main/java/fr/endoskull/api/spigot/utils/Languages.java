eb9d4a8532aa72350536e9b989c85d4b57df26ce
BebeDlaStreat <bebe.vandroux@gmail.com>
2022-06-20 09:34:41 +0200

    language slot

--- src/main/java/fr/endoskull/api/spigot/utils/Languages.java
+++ src/main/java/fr/endoskull/api/spigot/utils/Languages.java
@@ -7,17 +7,19 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public enum Languages {
-    FRENCH("http://textures.minecraft.net/texture/6903349fa45bdd87126d9cd3c6c0abba7dbd6f56fb8d78701873a1e7c8ee33cf", "Français", ""),
-    ENGLISH("http://textures.minecraft.net/texture/c439d7f9c67f32dcbb86b7010b1e14b60de96776a35f61cee982660aacf5264b", "English", "\n§7Translations may not be 100% accuracy but\n§7we do our best to ensure that they are.");
+    FRENCH("http://textures.minecraft.net/texture/6903349fa45bdd87126d9cd3c6c0abba7dbd6f56fb8d78701873a1e7c8ee33cf", "Français", "", 21),
+    ENGLISH("http://textures.minecraft.net/texture/c439d7f9c67f32dcbb86b7010b1e14b60de96776a35f61cee982660aacf5264b", "English", "\n§7Translations may not be 100% accuracy but\n§7we do our best to ensure that they are.", 23);
 
     private String skull;
     private String name;
     private String lore;
+    private int slot;
 
-    Languages(String skull, String name, String lore) {
+    Languages(String skull, String name, String lore, int slot) {
         this.skull = skull;
         this.name = name;
         this.lore = lore;
+        this.slot = slot;
     }
 
     public String getSkull() {
@@ -51,5 +53,9 @@ public enum Languages {
 
     public String getLore() {
         return lore;
+    }
+
+    public int getSlot() {
+        return slot;
     }
 }
